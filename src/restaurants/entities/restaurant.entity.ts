08fe7370e574c4a64ce0c9a296f8755478685b09
Heesang whang <hwhang0917@gmail.com>
2021-01-20 12:57:01 +0900

    :sparkles: Add Restaurant table by synchronizing typeORM with postgres

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,17 +1,31 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @Field((type) => Number)
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @Field((type) => String)
+  @Column()
   name: string;
 
   @Field((type) => Boolean, { nullable: true })
+  @Column()
   isGood?: boolean;
 
   @Field((type) => String)
+  @Column()
   address: string;
 
   @Field((type) => String)
+  @Column()
   owner: string;
+
+  @Field((type) => String)
+  @Column()
+  categoryName: string;
 }
