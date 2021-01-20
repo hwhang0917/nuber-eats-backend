e0979b6512439d067cbc7a47d670ae4c1f31c582
Heesang whang <hwhang0917@gmail.com>
2021-01-20 17:02:06 +0900

    :sparkles: Update DTO to become mapped types

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Field, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
@@ -11,21 +12,28 @@ export class Restaurant {
 
   @Field((type) => String)
   @Column()
+  @IsString()
+  @Length(5)
   name: string;
 
-  @Field((type) => Boolean, { nullable: true })
-  @Column()
+  @Field((type) => Boolean, { defaultValue: true })
+  @Column({ default: true })
+  @IsOptional()
+  @IsBoolean()
   isGood?: boolean;
 
   @Field((type) => String)
   @Column()
+  @IsString()
   address: string;
 
   @Field((type) => String)
   @Column()
+  @IsString()
   owner: string;
 
   @Field((type) => String)
   @Column()
+  @IsString()
   categoryName: string;
 }
