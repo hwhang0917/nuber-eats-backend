a044d99b3441bfd2055ae5ac59e3e3c65c3e3e6a
Heesang whang <hwhang0917@gmail.com>
2021-01-22 02:27:25 +0900

    :sparkles: Add editProfile mutation

---
+++ src/users/dtos/edit-profile.dto.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { InputType, ObjectType, PartialType, PickType } from '@nestjs/graphql';
+import { CoreOutput } from 'src/common/dtos/output.dto';
+import { User } from '../entities/user.entity';
+
+@ObjectType()
+export class EditProfileOutput extends CoreOutput {}
+
+@InputType()
+export class EditProfileInput extends PartialType(
+  PickType(User, ['email', 'password']),
+) {}
