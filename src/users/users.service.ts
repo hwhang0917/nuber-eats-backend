432108dd21b7472229accc97f079c98e45c5a0db
Heesang whang <hwhang0917@gmail.com>
2021-01-22 18:30:28 +0900

    :recycle: Refacotr User resolver & service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,8 +8,11 @@ import {
 import { LoginInput, LoginOutput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 import { JwtService } from 'src/jwt/jwt.service';
-import { EditProfileInput } from './dtos/edit-profile.dto';
+import { EditProfileInput, EditProfileOutput } from './dtos/edit-profile.dto';
 import { Verification } from './entities/verification.entity';
+import { UserProfileOutput } from './dtos/user-profile.dto';
+import { DeleteAccountOutput } from './dtos/delete-account.dto';
+import { VerifyEmailOutput } from './dtos/verify-email.dto';
 
 @Injectable()
 export class UsersService {
@@ -68,44 +71,68 @@ export class UsersService {
     }
   }
 
-  async findById(id: number): Promise<User> {
-    return this.users.findOne({ id });
+  async findById(id: number): Promise<UserProfileOutput> {
+    try {
+      const user = await this.users.findOne({ id });
+      if (user) {
+        return { ok: true, user };
+      }
+    } catch (error) {
+      return { ok: false, error: 'User not found' };
+    }
   }
 
   async editProfile(
     userId: number,
     { email, password }: EditProfileInput,
-  ): Promise<User> {
-    const user = await this.users.findOne(userId);
-    if (email) {
-      user.email = email;
-      user.verified = false;
-      await this.verifications.save(
-        this.verifications.create({
-          user,
-        }),
+  ): Promise<EditProfileOutput> {
+    try {
+      const user = await this.users.findOne(userId);
+      if (email) {
+        user.email = email;
+        user.verified = false;
+        await this.verifications.save(
+          this.verifications.create({
+            user,
+          }),
+        );
+      }
+      if (password) {
+        user.password = password;
+      }
+      await this.users.save(user);
+      return { ok: true };
+    } catch (error) {
+      return { ok: false, error: 'Could not update profile' };
+    }
+  }
+
+  async deleteUser(userId: number): Promise<DeleteAccountOutput> {
+    try {
+      const user = await this.users.findOne(userId);
+      if (user) {
+        await this.users.remove(user);
+      }
+      return { ok: true };
+    } catch (error) {
+      return { ok: false, error: 'Could not delete account' };
+    }
+  }
+
+  async verifyEmail(code: string): Promise<VerifyEmailOutput> {
+    try {
+      const verification = await this.verifications.findOne(
+        { code },
+        { relations: ['user'] },
       );
+      if (verification) {
+        verification.user.verified = true;
+        this.users.save(verification.user);
+        return { ok: true };
+      }
+      return { ok: false, error: 'Verification not found.' };
+    } catch (error) {
+      return { ok: false, error };
     }
-    if (password) {
-      user.password = password;
-    }
-    return this.users.save(user);
-  }
-
-  async deleteUser(userId: number): Promise<User> {
-    const user = await this.users.findOne(userId);
-    return this.users.remove(user);
-  }
-
-  async verifyEmail(code: string): Promise<boolean> {
-    const verification = await this.verifications.findOne(
-      { code },
-      { relations: ['user'] },
-    );
-    if (verification) {
-      verification.user.verified = true;
-      this.users.save(verification.user);
-    }
-    return false;
   }
 }
