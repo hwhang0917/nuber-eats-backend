ec2dacddf5bbe53feb730d4836d2b1e37b3d19df
Heesang whang <hwhang0917@gmail.com>
2021-01-28 19:51:48 +0900

    :white_check_mark: Add createAccount E2E test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,7 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
+
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -19,11 +28,70 @@ describe('UserModule (e2e)', () => {
     await getConnection().dropDatabase();
     await app.close();
   });
-  it.todo('createAccount');
+
+  describe('createAccount', () => {
+    const EMAIL = 'test@test.com';
+
+    it('should create an account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation {
+          createAccount(input:{
+            email:"${EMAIL}",
+            password:"test",
+            role:Admin
+          }) {
+            ok
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+        mutation {
+          createAccount(input:{
+            email:"${EMAIL}",
+            password:"test",
+            role:Admin
+          }) {
+            ok
+            error
+          }
+        }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toBe(
+            'There is a user with that email already',
+          );
+        });
+    });
+  });
+
   it.todo('userProfile');
+
   it.todo('login');
+
   it.todo('me');
+
   it.todo('verifyEmail');
+
   it.todo('editProfile');
+
   it.todo('deleteMyAccount');
 });
