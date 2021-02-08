eb546400027f99d3fa01e0d8cdedf73e3c4c791d
Heesang whang <hwhang0917@gmail.com>
2021-02-09 00:07:20 +0900

    :white_check_mark: Add createRestaruant unit test

--- src/restaurants/restaurants.service.spec.ts
+++ src/restaurants/restaurants.service.spec.ts
@@ -1,6 +1,8 @@
 import { Test } from '@nestjs/testing';
 import { getCustomRepositoryToken, getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateRestaurantInput } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 import { CategoryRepository } from './repositories/category.repository';
 import { RestaurantService } from './restaurants.service';
@@ -50,7 +52,63 @@ describe('Restaurants Service', () => {
   });
 
   describe('createRestaruant', () => {
-    it.todo('should create a new restaurant');
+    const createRestaurantArgs: CreateRestaurantInput = {
+      categoryName: 'Test Category',
+      name: 'test',
+      coverImage: 'test.jpg',
+      address: 'test',
+    };
+    const mockUser: User = null;
+
+    it('should create a new restaurant', async () => {
+      const mockCategory = {
+        name: createRestaurantArgs.categoryName,
+        icon: 'test.jpg',
+        slug: 'test-category',
+      };
+
+      const mockNewRestaurant = {
+        ...createRestaurantArgs,
+        ownerId: 0,
+        category: mockCategory,
+      };
+
+      restaurantsRepository.create.mockReturnValue(mockNewRestaurant);
+      categoriesRepository.getOrCreate.mockResolvedValue(mockCategory);
+
+      const result = await service.createRestaurant(
+        mockUser,
+        createRestaurantArgs,
+      );
+
+      expect(result).toEqual({ ok: true });
+      expect(restaurantsRepository.create).toHaveBeenCalled();
+      expect(restaurantsRepository.create).toHaveBeenCalledWith(
+        createRestaurantArgs,
+      );
+      expect(categoriesRepository.getOrCreate).toHaveBeenCalled();
+      expect(categoriesRepository.getOrCreate).toHaveBeenCalledWith(
+        createRestaurantArgs.categoryName,
+      );
+      expect(restaurantsRepository.save).toHaveBeenCalled();
+      expect(restaurantsRepository.save).toHaveBeenCalledWith(
+        mockNewRestaurant,
+      );
+    });
+
+    it('should fail on exception', async () => {
+      restaurantsRepository.save.mockRejectedValue(new Error());
+
+      const result = await service.createRestaurant(
+        mockUser,
+        createRestaurantArgs,
+      );
+
+      expect(result).toEqual({
+        ok: false,
+        error: 'Could not create restaurant',
+      });
+    });
   });
 
   describe('editRestaurant', () => {
