9c3eb5154038cf54fa5901a2edee729329d3569d
Heesang whang <hwhang0917@gmail.com>
2021-02-15 10:03:40 +0900

    :card_file_box: Add dish entity

---
+++ src/restaurants/entities/dish.entity.ts
@@ -0,0 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
+import { IsNumber, IsString, Length } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Column, Entity, ManyToOne, OneToMany, RelationId } from 'typeorm';
+import { Restaurant } from './restaurant.entity';
+
+@InputType('DishInputType', { isAbstract: true })
+@ObjectType()
+@Entity()
+export class Dish extends CoreEntity {
+  @Field((type) => String)
+  @Column()
+  @IsString()
+  @Length(5)
+  name: string;
+
+  @Field((type) => Int)
+  @Column()
+  @IsNumber()
+  price: number;
+
+  @Field((type) => String)
+  @Column()
+  @IsString()
+  photo: string;
+
+  @Field((type) => String)
+  @Column()
+  @IsString()
+  @Length(5, 140)
+  description: string;
+
+  @Field((type) => Restaurant, { nullable: true })
+  @ManyToOne((type) => Restaurant, (restaurant) => restaurant.menu, {
+    onDelete: 'CASCADE',
+  })
+  restaurant: Restaurant;
+
+  @RelationId((dish: Dish) => dish.restaurant)
+  restaurantId: number;
+}
