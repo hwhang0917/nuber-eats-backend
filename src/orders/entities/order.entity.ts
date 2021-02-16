7af46eba4f30d18f71c11ab040b6098458883785
Heesang whang <hwhang0917@gmail.com>
2021-02-16 19:43:02 +0900

    :card_file_box: Add order entity

---
+++ src/orders/entities/order.entity.ts
@@ -0,0 +1,71 @@
+import {
+  Field,
+  InputType,
+  Int,
+  ObjectType,
+  registerEnumType,
+} from '@nestjs/graphql';
+import { IsEnum, IsNumber, IsString, Length } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Dish } from 'src/restaurants/entities/dish.entity';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  RelationId,
+} from 'typeorm';
+
+export enum OrderStatus {
+  Pending = 'Pending',
+  Cooking = 'Cooking',
+  Delivering = 'Delivering',
+  Completed = 'Completed',
+}
+
+registerEnumType(OrderStatus, { name: 'OrderStatus' });
+
+@InputType('OrderInputType', { isAbstract: true })
+@ObjectType()
+@Entity()
+export class Order extends CoreEntity {
+  @Field((type) => User, { nullable: true })
+  @ManyToOne((type) => User, (user) => user.orders, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  customer?: User;
+
+  @Field((type) => User, { nullable: true })
+  @ManyToOne((type) => User, (user) => user.rides, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  driver?: User;
+
+  @Field((type) => Restaurant)
+  @ManyToOne((type) => Restaurant, (restaurant) => restaurant.orders, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  restaurant: Restaurant;
+
+  @Field((type) => [Dish])
+  @ManyToMany((type) => Dish)
+  @JoinTable()
+  dishes: Dish[];
+
+  @Field((type) => Int)
+  @Column()
+  @IsNumber()
+  total: number;
+
+  @Field((type) => OrderStatus)
+  @Column({ type: 'enum', enum: OrderStatus })
+  @IsEnum(OrderStatus)
+  status: OrderStatus;
+}
